3489f04cf0ae5c6edfbd50ebe51ec622d1fee201
Scott Milton <miltons@oregonstate.edu>
2015-12-10 19:14:16 -0800

    Add .c extension to program files. Add comment headers and main functions. Finish compileall script.

---
+++ program4/otp_enc_d.c
@@ -0,0 +1,62 @@
+/******************************************************************************
+ ** Filename:    otp_enc_d.c
+ ** Author:      Scott Milton
+ ** Date:        12/7/15
+ **
+ ** Description: This program 
+ **
+ ** Input:       from the keyboard: type char[]
+ **              from files:        type char[]
+ **
+ ** Output:      to the console and files : type char[], char*, const char*, int
+ ******************************************************************************/
+
+// #include <errno.h>     // for errno
+// #include <fcntl.h>     // for open
+// #include <limits.h>    // for INT_MAX
+// #include <signal.h>    // for sigset_t
+// #include <stdio.h>     // for fgets
+// #include <stdlib.h>    // for getenv, malloc, free
+// #include <string.h>    // for strcpy, strcat
+// #include <sys/stat.h>  // for stat 
+// #include <sys/types.h> // for pid_t
+// #include <sys/wait.h>  // for waitpid
+// #include <unistd.h>    // for exec
+
+// #define DEBUG             0 // change to 1 for debugging print statements 
+// #define MAX_ARGS        512 // maximum arguments accepted on command line
+// #define MAX_LENGTH     2048 // maximum length for a command line
+// #define MAX_PIDS       1000 // maximum PIDs to track
+
+// define bool as type
+typedef enum { false, true } bool;
+
+
+
+// declare global variables
+
+
+
+
+
+
+/******************************************************************************
+ ** Function:          
+ ** Description:       This function 
+ ** Parameters:        
+ ** Pre-Conditions:    
+ ** Post-Conditions:   
+ ******************************************************************************/
+// void functionName();
+
+
+
+
+
+int main(int argc, char** argv)
+{
+
+
+
+
+}
